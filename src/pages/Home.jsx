391d084cd36848eb446c9d1f27d9b2002a3adbf0
labinotsad <labinot.saadiku@gmail.com>
2024-10-08 17:19:06 +0300

    1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <section className='bg-black'>
+      <Navbar />
+      <Hero />
+    </section>
+  );
+};
+
+export default Home;
