47f50723cd5e5512b8243036382f58110be70c41
labinotsad <labinot.saadiku@gmail.com>
2024-10-19 16:24:59 +0300

    part30

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    assetsInlineLimit: 0, // Adjust this if you want to force files to be served as assets
+  },
+});
